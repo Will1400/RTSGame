52364a1064f147b4cc972a1efeb062572d5645f2
Will1400 <will1400@edu.campusvejle.dk>
2019-11-22 11:49:41 +0100

    Added base for damage system

---
+++ Assets/Scripts/Enums/DamageType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DamageType
+{
+    Direct,
+    LightMelee,
+    HeavyMelee,
+    LightRanged,
+    HeavyRanged
+}
---
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+
+    public float Defense { get; set; }
+    public DefenseType DefenseType { get; set; }
+
+    void Damage(float amount, DamageType damageType);
+}
