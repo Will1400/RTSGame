e6de4992d3b1b5b104ce4b544edc7dfe0ba07a91
Will1400 <will1400@edu.campusvejle.dk>
2019-11-25 08:57:57 +0100

    Added base HQ

---
+++ Assets/Scripts/Buildings/HQ.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HQ : MonoBehaviour, IDamageable
+{
+    public float Health { get; set; }
+
+    public float Defense { get; set; }
+    public DefenseType DefenseType { get; set; } = DefenseType.AllLight;
+
+    public void Damage(float amount, DamageType damageType)
+    {
+    }
+}
