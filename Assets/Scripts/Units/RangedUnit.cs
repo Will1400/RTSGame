51c0fc34987a200663355d316df09b7607f6104f
Will1400 <will1400@edu.campusvejle.dk>
2019-11-27 14:34:41 +0100

    Added base for ranged units

---
+++ Assets/Scripts/Units/RangedUnit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedUnit : Unit
+{
+    protected virtual void Update()
+    {
+        if (IsTargetOutOfRange())
+        {
+            target = null;
+            agent.ResetPath();
+        }
+
+        if (target != null)
+        {
+            if (CanAttackTarget())
+            {
+                AttackTarget();
+
+                if (agent.hasPath)
+                    agent.ResetPath();
+            }
+            else
+            {
+                MoveIntoAttackRange(target.position); 
+            }
+
+        }
+        else
+        {
+            GetNearbyTarget();
+        }
+    }
+}
