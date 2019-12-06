3714e9e5e3b06d6d01e8e8b3029e265bf0fb3fbf
Will1400 <will1400@edu.campusvejle.dk>
2019-12-06 10:23:41 +0100

    Added main menu

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private MultiplayerConnectionMenu multiplayerConnection;
+    [SerializeField]
+    private GameObject MainMenuPanel;
+    [SerializeField]
+    private GameObject serverBrowserPanel;
+
+    public void OnClickHost()
+    {
+        multiplayerConnection.Host();
+    }
+
+    public void OnClickServers()
+    {
+        GoToPanel(serverBrowserPanel.name);
+    }
+
+    void GoToPanel(string panelName)
+    {
+        serverBrowserPanel.SetActive(serverBrowserPanel.name.Equals(panelName));
+        MainMenuPanel.SetActive(MainMenuPanel.name.Equals(panelName));
+    }
+}
